d779ee01b67626d70ecdd00915501218d90d4759
Brunoevan <brunoevan42@gmail.com>
2025-05-09 22:02:50 +0000

    Nível Aventureiro-Inserção da Densidade Populacional e PiB-percapta

--- Super_trunfo.c
+++ Super_trunfo.c
@@ -3,7 +3,7 @@ int main(){
     // Nesta parte do programa são declarados os tipos e as variáveis utilizadas para cada carta do jogo.  
     char estado,codigo_carta[4],nome_cidade[20];
     int  populacao, numero_pontos_turisticos;
-    float area,PIB;
+    float area,PIB,densidade_populacional,PIB_percatpita;
 
     /*Esta parte do código é responsável por solicitar ao usuário que preencha as informações referentes à Carta_1, armazenando-as 
     nas respctivas variáveis*/
@@ -25,7 +25,8 @@ int main(){
      printf("Agora informe o PIB da cidade selecionada:\n");
      scanf("%f",&PIB);
      
-     //Esta parte do código imprimi na tela os dados informados pelo usuário para a Carta_1.
+     /*Esta parte do código imprimi na tela os dados informados pelo usuário para a Carta_1, calculando também a densidade populacional
+     e o PIB percapita*/
      printf("O Estado selecionado para a Carta_1 é: %c\n", estado);
      printf("O código Selecionado para a Carta_1 é: %s\n",codigo_carta);
      printf("O nome da Cidade para a Carta_1 é: %s\n",nome_cidade);
@@ -34,11 +35,16 @@ int main(){
      printf("A área da cidade em (Km²) para a Carta_1 é:%.2f ",area);
      printf("Km²\n");
      printf("O PIB da cidade selecionada para a Carta_1 é:%.2f",PIB);
-     printf(" bilhões de reais\n\n");
+     printf(" bilhões de reais\n");
+     densidade_populacional=(float) (populacao/area);
+     PIB_percatpita=(float)(PIB/populacao);
+     printf("O valor da densidade populacional para a Carta_1 é:%.2f",densidade_populacional);
+     printf(" hab/Km²\n");
+     printf("O valor do PIB_Percapita da cidade para a Carta_1 e´:%.2f",PIB_percatpita);
+     printf(" reais\n");
     
      /*Neste ponto do código é solicitado ao usuário que insira as informações referentes à Carta_2, 
-     armazenando-as nas respectivas variáveis*/
-    
+     armazenando-as nas respectivas variáveis*/    
      printf( "INSIRA OS DADOS DA CARTA_2.\n ");
 
      printf("Primeiro escolha entre os estados (A,B,C...ou H): \n");
@@ -56,7 +62,8 @@ int main(){
      printf("Agora informe o PIB da cidade selecionada:\n");
      scanf("%f",PIB); 
      
-     //Esta parte do código imprimi na tela os dados informados pelo usuário para a Carta_2.
+     /*Esta parte do código imprimi na tela os dados informados pelo usuário para a Carta_2, calculando também a densidade populacional
+     e o PIB percapita*/
      printf("O Estado selecionado para a Carta_2 é: %c\n", estado);
      printf("O Codigo selecionado para a Carta_2 é: %s\n",codigo_carta);
      printf("O nome da Cidade para a Carta_2 é: %s\n",nome_cidade);
@@ -66,6 +73,11 @@ int main(){
      printf("Km²\n");
      printf("O PIB da cidade selcionada para a Carta_2 é:%.2f",PIB);
      printf(" bilhões de reais\n");
-
-      return 0;  
+     densidade_populacional=(float) (populacao/area);
+     PIB_percatpita=(float)(PIB/populacao);
+     printf("O valor da densidade populacional para a Carta_2 é:%.2f",densidade_populacional);
+     printf(" hab/Km²\n");
+     printf("O valor do PIB_Percapita da cidade para a Carta_2 e´:%.2f",PIB_percatpita);
+     printf(" reais\n");
+     return 0;  
 }
